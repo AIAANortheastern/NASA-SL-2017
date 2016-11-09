d27b2af05729085af576a5284fe2a6f5bcdfbf3f
erronknight <erronknight@gmail.com>
2016-11-08 20:47:47 -0500

    first try at sensor suite code
    I tried

---
+++ first subscale iteration/2016_11_08_sensorsuite/2016_11_08_sensorsuite.ino
@@ -0,0 +1,195 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
+
+/* Assign a unique ID to the sensors */
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
+Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
+Adafruit_10DOF                dof   = Adafruit_10DOF();
+
+float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+
+void initSensors()
+{
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  if(!bmp.begin())
+  {
+    /* There was a problem detecting the BMP180 ... check your connections */
+    Serial.println("Ooops, no BMP180 detected ... Check your wiring!");
+    while(1);
+  }
+    displaySensorDetails();
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  
+  accel.getSensor(&sensor);
+  Serial.println(F("----------- ACCELEROMETER ----------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" m/s^2"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" m/s^2"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  gyro.getSensor(&sensor);
+  Serial.println(F("------------- GYROSCOPE -----------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" rad/s"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" rad/s"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" rad/s"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  
+  mag.getSensor(&sensor);
+  Serial.println(F("----------- MAGNETOMETER -----------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" uT"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" uT"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" uT"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+
+  bmp.getSensor(&sensor);
+  Serial.println(F("-------- PRESSURE/ALTITUDE ---------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" hPa"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" hPa"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution); Serial.println(F(" hPa"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensors_event_t event;
+ //
+ 
+}
+
+void getpitchrollheading() {
+   sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_event_t bmp_event;
+  sensors_vec_t   orientation;
+
+  /* Read the accelerometer and magnetometer */
+  accel.getEvent(&accel_event);
+  mag.getEvent(&mag_event);
+
+  /* Use the new fusionGetOrientation function to merge accel/mag data */  
+  if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation))
+  {
+    /* 'orientation' should have valid .roll and .pitch fields */
+    Serial.print(F("Orientation: "));
+    Serial.print(orientation.roll);
+    Serial.print(F(" "));
+    Serial.print(orientation.pitch);
+    Serial.print(F(" "));
+    Serial.print(orientation.heading);
+    Serial.println(F(""));
+  }
+
+  /* Previous code removed handling accel and mag data separately */
+  //  /* Calculate pitch and roll from the raw accelerometer data */
+  //  Serial.print(F("Orientation: "));
+  //  accel.getEvent(&accel_event);
+  //  if (dof.accelGetOrientation(&accel_event, &orientation))
+  //  {
+  //    /* 'orientation' should have valid .roll and .pitch fields */
+  //    Serial.print(orientation.roll);
+  //    Serial.print(F(" "));
+  //    Serial.print(orientation.pitch);
+  //    Serial.print(F(" "));
+  //  }
+  //  
+  //  /* Calculate the heading using the magnetometer */
+  //  mag.getEvent(&mag_event);
+  //  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
+  //  {
+  //    /* 'orientation' should have valid .heading data now */
+  //    Serial.print(orientation.heading);
+  //  }
+  //  Serial.println(F(""));
+}
+}
+
+void get_accel_mag_gyro_alt() {
+ sensors_event_t event;
+   
+  /* Display the results (acceleration is measured in m/s^2) */
+  accel.getEvent(&event);
+  Serial.print(F("ACCEL "));
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+
+  /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+  mag.getEvent(&event);
+  Serial.print(F("MAG   "));
+  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+
+  /* Display the results (gyrocope values in rad/s) */
+  gyro.getEvent(&event);
+  Serial.print(F("GYRO  "));
+  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");Serial.println("rad/s ");  
+
+  /* Display the pressure sensor results (barometric pressure is measure in hPa) */
+  bmp.getEvent(&event);
+  if (event.pressure)
+  {
+    /* Display atmospheric pressure in hPa */
+    Serial.print(F("PRESS "));
+    Serial.print(event.pressure);
+    Serial.print(F(" hPa, "));
+    /* Display ambient temperature in C */
+    float temperature;
+    bmp.getTemperature(&temperature);
+    Serial.print(temperature);
+    Serial.print(F(" C, "));
+    /* Then convert the atmospheric pressure, SLP and temp to altitude    */
+    /* Update this next line with the current SLP for better results      */
+    float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
+    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
+                                        event.pressure,
+                                        temperature)); 
+    Serial.println(F(" m"));
+  }
+}
+
